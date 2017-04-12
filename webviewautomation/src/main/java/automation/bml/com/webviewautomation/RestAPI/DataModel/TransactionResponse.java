f829ca2e872fa98024d9426895aabf5b01976477
Phoenix <phoenix214@protonmail.com>
2017-04-12 18:30:23 +0200

    Updates on project models

--- webviewautomation/src/main/java/automation/bml/com/webviewautomation/RestAPI/DataModel/TransactionResponse.java
+++ webviewautomation/src/main/java/automation/bml/com/webviewautomation/RestAPI/DataModel/TransactionResponse.java
@@ -7,5 +7,24 @@ import javax.annotation.Generated;
  */
 @Generated("org.jsonschema2pojo")
 public class TransactionResponse {
+    private Settings settings;
+    public Settings getSettings()
+    {
+        return settings;
+    }
+    public void setSettings(Settings settings)
+    {
+        this.settings = settings;
+    }
 
+    private Actions actions;
+    public Actions getActions()
+    {
+        return actions;
+    }
+
+    public void setActions(Actions actions)
+    {
+        this.actions = actions;
+    }
 }
