8f202303937a18063c8b06919fe5158cfa16be90
Phoenix <phoenix214@protonmail.com>
2017-04-23 23:58:28 +0200

    Updated the wifi detection module for latest SDK

--- webviewautomation/src/main/java/automation/bml/com/webviewautomation/WifiReceiver.java
+++ webviewautomation/src/main/java/automation/bml/com/webviewautomation/WifiReceiver.java
@@ -4,7 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 import static android.content.Context.CONNECTIVITY_SERVICE;
 
@@ -24,10 +26,31 @@ public class WifiReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        NetworkInfo info = null;
         ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if(mWifi != null && mWifi.isConnected() && isMobileEnabled) {
-            webview.updateData("UNABLE TO OBTAIN A 3G CONNECTION");
+        boolean isWifiEnabled = false;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Network[] networks = connManager.getAllNetworks();
+            for (Network network : networks) {
+                info = connManager.getNetworkInfo(network);
+                if (info != null) {
+                    if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+                        isWifiEnabled = true;
+                        break;
+                    }
+                }
+            }
+        } else {
+            info = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            if (info != null)
+                isWifiEnabled = true;
+            
+        }
+       
+        if(isWifiEnabled)
+        {
+            if(info.isConnected() && isMobileEnabled)
+                webview.updateData("UNABLE TO OBTAIN A 3G CONNECTION");
         }
     }
 }
